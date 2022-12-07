180c36ff62e6b2bc3ff952655799b171e4aa17bf
Andrea Amaya <andreaa.malin@gmail.com>
2022-12-06 22:14:12 -0600

    xylophone app + challenge

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,16 +6,33 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        transitionOnPress(button: sender)
+        playSound(letter: sender.currentTitle!)
+    }
+    
+    func playSound(letter: String) {
+        let url = Bundle.main.url(forResource: letter, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        
+        player.play()
+    }
+    
+    func transitionOnPress(button: UIButton) {
+        button.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            button.alpha = 1
+        }
+    }
 }
 
